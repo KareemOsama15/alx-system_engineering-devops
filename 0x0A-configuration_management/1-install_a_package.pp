ffc0472e6745339167201f262569e2825d157297
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-20 04:16:26 +0200

    [push] project 0x0A-configuration_management tasks[0:2]

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
 # install flask from pip3
+
+package { 'python3-pip':
+ensure => 'installed',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
