1f1963be530d44236f221dcf286a97a1b69b688f
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-01-30 04:30:25 +0200

    [push] project tasks[2]

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,26 @@
 # Add a custom HTTP header with Puppet
-
-exec { 'update packages':
-    provider => shell,
-    command  => 'sudo apt-get update',
-    before   => Exec['install nginx'],
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-exec { 'install nginx':
-    provider => shell,
-    command  => 'sudo apt-get -y install nginx',
-    before   => Exec['add header'],
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-exec { 'add header':
-    provider => shell,
-    command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-    before   => Exec['retsart nginx']
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-exec { 'retsart nginx':
-    provider => shell,
-    command  => 'sudo service nginx retsart'
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
 
 file_line { ' creating a custom HTTP header response':
