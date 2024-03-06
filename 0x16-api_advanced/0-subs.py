b998a58c8a1970076b09b660640e9bbea383c89a
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-03-06 04:44:45 +0200

    [push] project tasks[0:2]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,18 +7,17 @@ def number_of_subscribers(subreddit):
     """
     function queries the Reddit API
     and returns the number of subscribers"""
-    pass
 
-    # url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # headers = {
-    #     'User-Agent': 'user-agent'
-    # }
-    # response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'user-agent'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # if response.status_code != 200:
-    #     return 0
+    if response.status_code != 200:
+        return 0
 
-    # data = response.json()
-    # subscribers = data['data']['subscribers']
+    data = response.json()
+    subscribers = data['data']['subscribers']
 
-    # return subscribers
+    return subscribers
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,25 +7,24 @@ def top_ten(subreddit):
     """
     function queries the Reddit API and prints
     the titles of the first 10 hot posts"""
-    pass
 
-    # url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    # headers = {
-    #     "User-Agent": "user-agent"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        "User-Agent": "user-agent"
+    }
+    # params = {
+    #     'limit': 10
     # }
-    # # params = {
-    # #     'limit': 10
-    # # }
-    # response = requests.get(url,
-    #                         headers=headers,
-    #                         # params=params,
-    #                         allow_redirects=False)
+    response = requests.get(url,
+                            headers=headers,
+                            # params=params,
+                            allow_redirects=False)
 
-    # if response.status_code != 200:
-    #     print(None)
-    #     return
+    if response.status_code != 200:
+        print(None)
+        return
 
-    # data = response.json()
-    # posts = data['data']['children']
-    # for post in posts:
-    #     print(post['data']['title'])
+    data = response.json()
+    posts = data['data']['children']
+    for post in posts:
+        print(post['data']['title'])
