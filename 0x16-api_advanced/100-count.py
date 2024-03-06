2a81892905b53c070906df82e89b501f98e7fb3e
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-03-06 04:40:00 +0200

    [push] project tasks[0:2]

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Module of count_words function"""
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    recursive function that queries the Reddit API, parses the title
+    of all hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces.
+    """
+    pass
