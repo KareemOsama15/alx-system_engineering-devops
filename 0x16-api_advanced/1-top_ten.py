992aa4b1912de74a4b4c049490950e13b013afd1
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-03-06 04:42:41 +0200

    [push] project tasks[0:2]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,24 +7,25 @@ def top_ten(subreddit):
     """
     function queries the Reddit API and prints
     the titles of the first 10 hot posts"""
+    pass
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {
-        "User-Agent": "user-agent"
-    }
-    # params = {
-    #     'limit': 10
+    # url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    # headers = {
+    #     "User-Agent": "user-agent"
     # }
-    response = requests.get(url,
-                            headers=headers,
-                            # params=params,
-                            allow_redirects=False)
+    # # params = {
+    # #     'limit': 10
+    # # }
+    # response = requests.get(url,
+    #                         headers=headers,
+    #                         # params=params,
+    #                         allow_redirects=False)
 
-    if response.status_code != 200:
-        print(None)
-        return
+    # if response.status_code != 200:
+    #     print(None)
+    #     return
 
-    data = response.json()
-    posts = data['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    # data = response.json()
+    # posts = data['data']['children']
+    # for post in posts:
+    #     print(post['data']['title'])
